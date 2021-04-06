b21df57c52230ce675779434d59002f2b0e444d8
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-06 07:43:09 -0600

    API Data implementation in Table

--- src/app/components/score-card-table/score-card-table.component.ts
+++ src/app/components/score-card-table/score-card-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { GetAPIService } from '../../services/get-api.service';
+import { ComplexCourse } from 'src/app/interfaces/complex-course';
+import { Data } from 'src/app/interfaces/data';
+import { GetAPIService } from 'src/app/services/get-api.service';
 
 @Component({
   selector: 'app-score-card-table',
@@ -9,11 +11,19 @@ import { GetAPIService } from '../../services/get-api.service';
 export class ScoreCardTableComponent implements OnInit {
   constructor(private api: GetAPIService) {}
 
+  courseData;
+
   ngOnInit(): void {
     this.api.apiCall('18300').subscribe((data) => {
       console.warn('get API data', data);
-      let coursesInfo = data;
-      console.log(coursesInfo);
+      console.log(data);
+      let reformattedData = data as Data;
+      this.setup(reformattedData.data as ComplexCourse);
     });
+  }
+  setup(data: ComplexCourse): void {
+    console.warn(data);
+    this.courseData = data;
+    console.warn(this.courseData.courseId);
   }
 }
