daa03077c36887659d764ee54c87678137590ff1
Gideon Jones <gideon.jones8310@stu.mtec.edu>
2021-04-08 13:49:15 -0600

    Auto-Filling TeeBoxes, PlayerRows generated on pre-set variable

--- src/app/components/score-card-table/score-card-table.component.ts
+++ src/app/components/score-card-table/score-card-table.component.ts
@@ -11,19 +11,85 @@ import { GetAPIService } from 'src/app/services/get-api.service';
 export class ScoreCardTableComponent implements OnInit {
   constructor(private api: GetAPIService) {}
 
+  playerCount = 8;
+  halfPlayerNumb = this.playerCount - Math.round(this.playerCount / 2);
   courseData;
+  teeBoxElement;
+  html = '';
+  frontPlayers = [];
+  backPlayers = [];
 
   ngOnInit(): void {
-    this.api.apiCall('18300').subscribe((data) => {
-      console.warn('get API data', data);
-      console.log(data);
+    for (let i = 0; i < this.playerCount; i++) {
+      if (i < this.halfPlayerNumb) {
+        this.frontPlayers.push(i + 1);
+      } else {
+        this.backPlayers.push(i + 1);
+      }
+    }
+    this.api.apiCall('11819').subscribe((data) => {
       let reformattedData = data as Data;
       this.setup(reformattedData.data as ComplexCourse);
     });
   }
   setup(data: ComplexCourse): void {
-    console.warn(data);
     this.courseData = data;
-    console.warn(this.courseData.courseId);
+    for (let i = 0; i < this.courseData.holes[0].teeBoxes.length; i++) {
+      this.html += this.makeTeeBoxRows(this.courseData, i);
+    }
+    this.findTeeBoxElem();
   }
+
+  makeTeeBoxRows(data, teeBoxNumb): string {
+    if (data.holes) {
+      console.log(teeBoxNumb);
+      // console.log(data.holes);
+      let html = ``;
+      let inTot = 0,
+        outTot = 0;
+      let holeCount = data.holes.length;
+      let holesFront = holeCount - Math.round(holeCount / 2) - 1;
+      // console.log(`Teebox ${teeBoxNumb}`);
+      html += `<tr style="background-color:${data.holes[0].teeBoxes[teeBoxNumb].teeHexColor}`;
+      if (data.holes[0].teeBoxes[teeBoxNumb].teeColorType == 'black')
+        html += `; color: white`;
+      html += `"><th style="text-transform: uppercase; text-align: center; padding: 15px; border: 1px solid black; border-collapse: collapse;">${data.holes[0].teeBoxes[teeBoxNumb].teeColorType}</th>`;
+      for (let j = 0; j <= holesFront; j++) {
+        html += `<td style="text-align: center; padding: 15px; border: 1px solid black; border-collapse: collapse;">${data.holes[j].teeBoxes[teeBoxNumb].yards}`;
+        inTot += data.holes[j].teeBoxes[teeBoxNumb].yards;
+      }
+      html += `<th style="text-align: center; padding: 15px; border: 1px solid black; border-collapse: collapse;">${inTot}</th>`;
+      for (let j = holesFront + 1; j < holeCount; j++) {
+        html += `<td style="text-align: center; padding: 15px; border: 1px solid black; border-collapse: collapse;">${data.holes[j].teeBoxes[teeBoxNumb].yards}`;
+        outTot += data.holes[j].teeBoxes[teeBoxNumb].yards;
+      }
+      html += `<th style="text-align: center; padding: 15px; border: 1px solid black; border-collapse: collapse;">${outTot}</th>`;
+      html += `<th style="text-align: center; padding: 15px; border: 1px solid black; border-collapse: collapse;">${
+        inTot + outTot
+      }</th>
+        </tr>`;
+      return html;
+    } else {
+      this.makeTeeBoxRows(data, teeBoxNumb);
+      return '';
+    }
+  }
+
+  findTeeBoxElem(): void {
+    const el = document.getElementById('courseTable');
+    if (el) {
+      console.log(el);
+      sleep(500).then(() => {
+        el.innerHTML += this.html;
+      });
+    } else {
+      sleep(2000).then(() => {
+        this.findTeeBoxElem();
+      });
+    }
+  }
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
